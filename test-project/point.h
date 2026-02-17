533d2c0af1d79ad6c374bf4a06776ddb72ac3b14
germa <Germanzhebrik@gmail.com>
2026-02-17 18:21:49 +0300

    New function DevBranch

--- test-project/point.h
+++ test-project/point.h
@@ -1,6 +1,12 @@
 #pragma once
+#include <iostream>
 
 struct Point{
   double x;
   double y;
 };
+
+// ????????? ? ????? dev 
+void DevBranch() {
+	std::cout << "Working on branch develop!" << std::endl;
+}
