02f61ea72a554ffb785fd12855adc0274f07beb3
germa <Germanzhebrik@gmail.com>
2026-02-18 18:55:08 +0300

    New func to check stash func

--- test-project/point.h
+++ test-project/point.h
@@ -16,6 +16,10 @@ int SumModule2(int a, int b) {
 	return (a + b) % 2;
 }
 
+int NewFucn(const int& b) {
+	return b;
+}
+
 void VersionInMainBranch() {
 	std::cout << "For merge with develop branch!" << std::endl;
 }
